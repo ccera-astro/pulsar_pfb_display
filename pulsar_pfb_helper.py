9c1d568b4df69c981fc4e3803432befef622d6c3
Marcus D. Leech <mleech@ripnet.com>
2016-11-15 21:33:59 -0500

    Added ability to compute/log total power as well (cheap might as well) Added --dmult (integer) parameter for scaling the delays -- 0 disables

--- pulsar_pfb_helper.py
+++ pulsar_pfb_helper.py
@@ -1,5 +1,11 @@
 # this module will be imported in the into your flowgraph
+import sys
+import os
+import time
+import ephem
+import operator
 import math
+
 
 def f_pad(filt,nc):
     f = list(filt)
@@ -14,7 +20,7 @@ def f_pad(filt,nc):
     return([0.0]*low+f+[0.0]*high)
 
 
-def calculate_delays(dm,freq,bw,nchan,drate):
+def calculate_delays(dm,freq,bw,nchan,drate,mult):
 
     f_lower = freq-(bw/2.0)
     f_upper = freq+(bw/2.0)
@@ -49,7 +55,7 @@ def calculate_delays(dm,freq,bw,nchan,drate):
         z = float(d) * perchan
         z = round(z)
         z = int(z)
-        delays.append(z)
+        delays.append(z*mult)
     
     return (delays)
 
@@ -57,16 +63,41 @@ import  time
 import sys
 import os
 
-def log(vec,pref):
+def log(vec,pref,longitude,which):
     ltp = time.gmtime()
-    fn = pref + "%04d%02d%02d.csv" % (ltp.tm_year, ltp.tm_mon, ltp.tm_mday)
+    fn = pref + "-profile-%04d%02d%02d.csv" % (ltp.tm_year, ltp.tm_mon, ltp.tm_mday)
+    if (which == 1):
+        fn = pref+"-tp-%04d%02d%02d.csv" % (ltp.tm_year, ltp.tm_mon, ltp.tm_mday)
     f = open(fn, "a")
-    f.write("%02d,%02d,%02d," % (ltp.tm_hour, ltp.tm_min, ltp.tm_sec))
-    for val in vec:
-        f.write("%.6f," % val)
+    curs = cur_sidereal(longitude)
+    f.write("%02d,%02d,%02d,%s," % (ltp.tm_hour, ltp.tm_min, ltp.tm_sec, curs))
+    if which == 0:
+        for val in vec:
+            f.write("%.6f," % val)
+    else:
+        f.write("%11.8f" % vec)
     f.write("\n")
     f.close()
     return 0
 
+def cur_sidereal(longitude):
+    longstr = "%02d" % int(longitude)
+    longstr = longstr + ":"
+    longitude = abs(longitude)
+    frac = longitude - int(longitude)
+    frac *= 60
+    mins = int(frac)
+    longstr += "%02d" % mins
+    longstr += ":00"
+    x = ephem.Observer()
+    x.date = ephem.now()
+    x.long = longstr
+    jdate = ephem.julian_date(x)
+    tokens=str(x.sidereal_time()).split(":")
+    hours=int(tokens[0])
+    minutes=int(tokens[1])
+    seconds=int(float(tokens[2]))
+    sidt = "%02d,%02d,%02d" % (hours, minutes, seconds)
+    return (sidt)
 
     
